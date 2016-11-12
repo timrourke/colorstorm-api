5d402149fc3dcbc5ff290f549d7c221b2ce21e21
Tim Rourke <tim@timrourke.com>
2016-11-12 08:31:25 -0600

    Reverting exrm version

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Colorstorm.Mixfile do
       {:comeonin, "~> 2.5"},
       {:cors_plug, "~> 1.1"},
       {:cowboy, "~> 1.0"},
-      {:exrm, "~> 1.0.8"},
+      {:exrm, "~> 0.15.3"},
       {:gettext, "~> 0.9"},
       {:guardian, "~> 0.13.0"},
       {:ja_serializer, "~> 0.10.0"},
